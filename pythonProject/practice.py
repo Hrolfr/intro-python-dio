8a6423d1bb7bc674d149fa49450151371d878e05
Hrolfr <sabrina.sales163@gmail.com>
2021-11-28 19:31:17 -0300

    coisas

---
+++ pythonProject/practice.py
@@ -0,0 +1,25 @@
+#Não lembro o nome
+
+# a = 6
+# b = 8
+# soma = a + b
+# print('A soma é: ' +  str(soma))
+
+#If
+
+# batata = 'saborosa'
+# if batata == 'horrível':
+#     print('credo!')
+# else:
+#    print('Que delicia!')
+
+# dia =  'bom'
+# if dia == 'bom':
+#     print('Incrível, fico feliz!')
+# else:
+#     print('O que houve?')
+
+
+
+
+
