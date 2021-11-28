fd698103bf53aada9d120de95b5cacbe63b88f9a
Hrolfr <sabrina.sales163@gmail.com>
2021-11-28 18:59:48 -0300

    coisas

---
+++ pythonProject/aula2.py
@@ -0,0 +1,23 @@
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('soma: {}. subtracao: {}'.format(soma, subtracao))#se eu inverter o nome, fica errado
+print('soma: {soma}. subtracao: {subtracao}'.format(soma, subtracao)) #dá erro
+# print ('soma:' + str(soma))
+# print ('subtracao:' + str(subtracao))
+# print ('multiplicacao:'+ str(multiplicacao))
+# print ('divisao:' + str(divisao))
+print (soma)
+print (subtracao)
+print (multiplicacao)
+print (int(divisao))
+print (divisao)
+print (resto)
+x = '1'
+#soma2 = x + 1  assim dá erro TypeError: can only concatenate str (not "int") to str
+# soma2 = int(x) + 1
+# print(soma2)
