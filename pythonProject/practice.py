164e351712564072dde9c8cdd690d28722a9191e
Hrolfr <sabrina.sales163@gmail.com>
2021-12-06 20:12:39 -0300

    coisas

--- pythonProject/practice.py
+++ pythonProject/practice.py
@@ -1,9 +1,51 @@
-#Não lembro o nome
+#Operações
 
 # a = 6
 # b = 8
 # soma = a + b
 # print('A soma é: ' +  str(soma))
+
+# S = 10
+# Se = 10
+# multiplicacao = S * Se
+# print('o resultado é obviamente: ' + str(multiplicacao))
+
+# A = 500
+# B = 3
+# divisao = A % B
+# print(divisao) retorna o resto da divisão
+
+#curiosidades:
+# A = 500
+# B = 3
+# divisao = A // B
+# print(divisao) não me devolve o tal do resto do float
+
+# A = 5 * 3 + 2
+# print(A) nesse ele prioriza a multiplicação mesmo sem eu dizer, e me devolve 17
+#
+# A = 5 * (3 + 2)
+# print(A) nesse a prioridade é a adição, então ele me dá 25
+
+#aqui
+
+# usar só:
+# n
+# me devolve:
+# Traceback (most recent call last):
+#   File "C:/Users/Sales/programming/cursos-sabrina/DIO/intro-phyton-dio/pythonProject/practice.py", line 13, in <module>
+#     n
+# NameError: name 'n' is not defined
+
+#list
+# arr = [0, 2, 4, 6, 8]
+# print(arr[2])
+#
+# arr1 =  [0, 2, 4, 6, 8]
+# print(arr[4])
+#
+# katze = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(katze[6]) fiz certin
 
 #If
 
@@ -19,7 +61,38 @@
 # else:
 #     print('O que houve?')
 
+#  x = int(input("Please enter an integer: "))
+# Please enter an integer: 42:
+#  if x < 0:
+#      x = 0
+#      print('Negative changed to zero')
+#  elif x == 0:
+#      print('Zero')
+#  elif x == 1:
+#     print('Single')
+
+# x = 12
+# if x > 20:
+#     print('nein')
+# elif x == 12:
+#     print('Zwolf')
+
+# z = 'zebra'
+# if z != 'zebra':
+#     print('cavalo?')
+# elif z == 'zebra':
+#     print('fofinha hihi')
+
+#prática depois da observação de hoje
+
+# b = 5 * 4
+# while b == 20:
+#     print('Du bist klug')
 
 
-
+c = 4 * 9 + 2
+if c > 32:
+    print('cool')
+else:
+    print('shut up bitch')
 
